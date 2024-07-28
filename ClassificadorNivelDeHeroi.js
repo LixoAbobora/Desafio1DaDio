324af48d9e773a6187a36d8b3f2c1393c1f1e68c
LixoAbobora <lixoabobora@gmail.com>
2024-07-28 16:57:42 -0300

    Adicionando o arquivo .js

---
+++ ClassificadorNivelDeHeroi.js
@@ -0,0 +1,24 @@
+// Solicita o nome e a quantidade de experiência do herói.
+let nome = prompt("Digite o nome do herói:");
+let xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
+// Verifica o nível do herói com base na quantia de xp.
+let nivel;
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1000 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp > 2000 && xp <= 5000) {
+  nivel = "Prata";
+} else if (xp > 5000 && xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp > 7000 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp > 8000 && xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp > 9000 && xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+// Exibe a mensagem com o nome e o nível do herói respectivamente.
+alert(`O Herói de nome **${nome}** está no nível de **${nivel}**`);
